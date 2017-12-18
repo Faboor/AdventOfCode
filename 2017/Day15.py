6aba47923b8b6b1e87db10af651a0e3b807ed4dc
Peter Kovary <peter.kovary@hotmail.com>
2017-12-18 13:49:40 +0000

    Day15

--- 2017/Day15.py
+++ 2017/Day15.py
@@ -15,3 +15,52 @@
 # instead uses a specific starting value as its "previous
 # value" (as listed in your puzzle input).
 # After 40 million pairs, what is the judge's final count?
+M = 2147483647
+mask = 65535
+
+
+class Gen(object):
+  def __init__(self, start, factor, filter):
+    self.previous = start
+    self.factor = factor
+    self.filter = filter
+
+  def __next__(self):
+    self.previous = (self.previous * self.factor) % M
+    while self.previous % self.filter:
+      self.previous = (self.previous * self.factor) % M
+    return self.previous
+
+
+def pairs(A, B, limit):
+  count = 0
+  for i in range(limit):
+    count += judge(next(A), next(B))
+  return count
+
+
+def judge(a, b):
+  return (a & mask) == (b & mask)
+
+
+def init_gens(filter_a=1, filter_b=1):
+  A = Gen(512, 16807, filter_a)
+  B = Gen(191, 48271, filter_b)
+  return A, B
+
+
+# --- Part Two ---
+# They still generate values in the same way, but now they
+# only hand a value to the judge when it meets their
+# criteria:
+# - Generator A looks for values that are multiples of 4.
+# - Generator B looks for values that are multiples of 8.
+# Each generator functions completely independently
+# After 5 million pairs, but using this new generator logic,
+# what is the judge's final count?
+
+# Modified part 1 to allow filters and different limits
+
+if __name__ == '__main__':
+  print(pairs(*init_gens(), limit=(40 * 10**6)))
+  print(pairs(*init_gens(4, 8), limit=(5 * 10**6)))
