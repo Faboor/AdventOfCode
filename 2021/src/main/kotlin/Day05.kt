384d7e770756210f8d09608ff5de53ca19db681a
Peter Kovary <kovary.p23@gmail.com>
2021-12-07 11:06:12 +0000

    [2021] Day05

---
+++ 2021/src/main/kotlin/Day05.kt
@@ -0,0 +1,65 @@
+import utils.Input
+
+class Day05 private constructor() {
+  companion object {
+    data class Dir(val dx: Int, val dy: Int) {
+      operator fun plus(other: Dir) = Dir(dx + other.dx, dy + other.dy)
+    }
+    data class Point(val x: Int, val y: Int) {
+      operator fun plus(dir: Dir) = Point(x + dir.dx, y + dir.dy)
+    }
+    data class Line(val start: Point, val end: Point): Iterable<Point> {
+      val dir = Dir(
+        (end.x - start.x).coerceIn(-1, 1),
+        (end.y - start.y).coerceIn(-1, 1),
+      )
+
+      override fun iterator() = object : Iterator<Point> {
+          var current = start
+          override fun hasNext() = current != end + dir
+          override fun next() = current.also { current += dir }
+      }
+    }
+
+    private const val SIZE = 1000
+
+    private fun Input.asLines() = lineSequence()
+      .flatMap { it.splitToSequence(" -> ") } // to point strings
+      .flatMap { it.splitToSequence(',') } // to individual coors
+      .map { it.toInt() }
+      .windowed(2, 2)
+      .map { Point(it[0], it[1]) }
+      .windowed(2, 2)
+      .map { Line(it[0], it[1]) }
+
+    private operator fun Array<IntArray>.get(point: Point) =
+      get(point.y)[point.x]
+    private operator fun Array<IntArray>.set(point: Point, value: Int) {
+      get(point.y)[point.x] = value
+    }
+
+    private fun countIntersections(linesSequence: Sequence<Line>): Int {
+      val board = Array(SIZE) { IntArray(SIZE) }
+      var counter = 0
+      return linesSequence
+        .flatMap { it.iterator().asSequence() }
+        .onEach { board[it] += 1 }
+        .sumOf { if (board[it] == 2) 1 else 0 as Int }
+    }
+
+    fun part1(input: Input): Int {
+      return countIntersections(
+        input.asLines().filter { it.dir.dx == 0 || it.dir.dy == 0 })
+    }
+
+    fun part2(input: Input): Int {
+      return countIntersections(input.asLines())
+    }
+  }
+}
+
+fun main() {
+  val input = Input("day05")
+  println(Day05.part1(input))
+  println(Day05.part2(input))
+}
---
+++ 2021/src/test/kotlin/Day05Test.kt
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import utils.Input
+
+internal class Day05Test {
+
+  private val input = Input("day05")
+
+  @Test
+  fun part1Test() {
+    expectThat(Day05.part1(input)).isEqualTo(5)
+  }
+
+  @Test
+  fun part2Test() {
+    expectThat(Day05.part2(input)).isEqualTo(12)
+  }
+}
